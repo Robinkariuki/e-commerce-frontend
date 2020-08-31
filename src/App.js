18066e46fc4bfc0c82aedfff4c736fddf438a915
Robinkariuki <robinkaris7@gmail.com>
2020-08-31 16:12:04 +0300

    installed axios

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ return(
             <Products/>
     </Route>
 </Router>
-
 )
 
 }
--- src/shared/Navigation/MainNavbar.js
+++ src/shared/Navigation/MainNavbar.js
@@ -1,21 +1,22 @@
 import React from  'react';
 import {NavLink} from 'react-router-dom';
+import './MainNavbar.css'
 
 
 const MainNavigation =()=>{
   
     return(
-<nav className="navbar  heavy-rain-gradient ">
+<nav className="navbar mean-fruit-gradient ">
     <ul className="navbar-nav mr-auto">
-    <NavLink to="/">
+    <NavLink to="/" className="linkText">
     <li className="navbar-brand">Market place</li> 
     </NavLink>
     </ul>
   <ul>
-  <NavLink to="/cart">
+  <NavLink to="/cart" className="linkText">
   <i className="fas fa-shopping-cart"style={{marginRight:'5em'}}></i>
   </NavLink>
-  <NavLink to ="profile">
+  <NavLink to ="profile" className="linkText">
    <i className="far fa-user"></i>
   </NavLink>
   </ul>
