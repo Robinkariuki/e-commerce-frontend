d683ff341bb6852da7edc8e649d406268c23de07
Robinkariuki <robinkaris7@gmail.com>
2020-09-02 22:15:40 +0300

    made changes

---
+++ src/products/components/productList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ProductItem from './productItem';
+import './productList.css'
+
+
+const ProductList =(props)=>{
+    if(props.items.length===0){
+        return(
+            <div className="center">
+                
+                <h2>No Images</h2>
+               
+
+            </div>
+        )
+    } 
+  
+    return(<div className='id'>
+        
+           { props.items.map(item=>(
+              <ProductItem 
+              className={'id'} 
+              key={item.id}
+              id={item.id}
+              image={item.urls}
+              alt={item.alt_description}
+              likes={item.likes}
+              description={item.description}
+              
+              
+              
+              />
+           ))}
+        </div>
+    )
+}
+
+
+export default ProductList;
--- src/products/pages/products.js
+++ src/products/pages/products.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
+import ProductList from '../components/productList';
 import MainNavigation from '../../shared/Navigation/MainNavbar';
-
+const Unsplash = require('unsplash-js').default;
 
 const Products =()=>{
+
+    const [loadedUsers,setLoadedUsers] = useState([])
+    const unsplash = new Unsplash({ accessKey: "CGU__vWPrr2KnXkILVwZzWT6PwsFxpcsL8iDqiyRoz8" });
+
+    useEffect(()=>{
+    unsplash.users.profile("robin")
+    .catch(err => {
+    console.log(err)
+    });
+    unsplash.photos.listPhotos(1, 15, "latest")
+    .then(res=>res.json())
+    .then(results=>{
+      setLoadedUsers(results)
+      console.log(loadedUsers)
+      console.log(results)
+    })
+    .catch((error) => {
+        console.log(error)
+      })
+
+    },[])
     return(
      
+     <React.Fragment>
     <MainNavigation/>
+    <ProductList items={loadedUsers}/>
+    </React.Fragment>
   
     )
 }
